d3e86be8b28f79ca04f83d9d3455521dbf19a60c
Stefano Pigozzi <me@steffo.eu>
2021-04-03 16:17:05 +0200

    🐛 Remove namespace= from path
    It seems like it's no longer supported

--- sophon/urls.py
+++ sophon/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include("rest_framework.urls"), namespace="rest_framework")
+    path('api-auth/', include("rest_framework.urls"))
 ]
