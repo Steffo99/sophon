1c28a89dfae84d5412a34548d826bd8c6d20d029
Stefano Pigozzi <me@steffo.eu>
2021-04-05 18:30:03 +0200

    🔧 Register models in the admin page

--- sophon/core/admin.py
+++ sophon/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.Project)
+admin.site.register(models.DataSource)
--- sophon/settings.py
+++ sophon/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'sophon.core',
+    'sophon.core',  # FIXME: Is .apps.CoreConfig not needed?
 ]
 
 MIDDLEWARE = [
