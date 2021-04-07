111e22d63b68d788cafa5568110361a89ccd6470
Stefano Pigozzi <me@steffo.eu>
2021-04-07 20:39:15 +0200

    ✨ Add sync action to DataSource viewset

--- sophon/core/urls.py
+++ sophon/core/urls.py
@@ -6,6 +6,7 @@ from . import views
 router = DefaultRouter()
 router.register("projects", views.ProjectViewSet)
 router.register("datasources", views.DataSourceViewSet)
+router.register("dataflows", views.DataFlowViewSet)
 
 
 urlpatterns = [
