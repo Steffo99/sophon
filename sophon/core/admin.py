d748ca272bfd42d440f9b928f497e7dd362f0a62
Stefano Pigozzi <me@steffo.eu>
2021-04-08 14:24:08 +0200

    ✨ Make a common CoreAdmin class

--- sophon/core/admin.py
+++ sophon/core/admin.py
@@ -2,16 +2,22 @@ from django.contrib import admin
 from . import models
 
 
+class CoreAdmin(admin.ModelAdmin):
+    """
+    :class:`django.contrib.admin.ModelAdmin` class from which all other admin classes inherit.
+    """
+
+
 @admin.register(models.Project)
-class ProjectAdmin(admin.ModelAdmin):
+class ProjectAdmin(CoreAdmin):
     pass
 
 
 @admin.register(models.DataSource)
-class DataSourceAdmin(admin.ModelAdmin):
+class DataSourceAdmin(CoreAdmin):
     pass
 
 
 @admin.register(models.DataFlow)
-class DataFlowAdmin(admin.ModelAdmin):
+class DataFlowAdmin(CoreAdmin):
     pass
