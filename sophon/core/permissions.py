c6abc1bd0eeab4444beb1ff1d938b3b7220d17d2
Stefano Pigozzi <me@steffo.eu>
2021-04-15 17:45:30 +0200

    🔧 Split ProjectViewSet in multiple sub-viewsets

--- sophon/core/permissions.py
+++ sophon/core/permissions.py
@@ -4,15 +4,16 @@ from rest_framework import permissions
 log = logging.getLogger(__name__)
 
 
-class ProjectPermissions(permissions.BasePermission):
+class CanViewObject(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if view.action == "retrieve":
-            return obj.can_be_viewed_by(request.user)
-        elif view.action == "update":
-            return obj.can_be_edited_by(request.user)
-        elif view.action == "partial_update":
-            return obj.can_be_edited_by(request.user)
-        elif view.action == "destroy":
-            return obj.can_be_administrated_by(request.user)
-        log.warning(f"Rejecting permissions for unknown action: {view.action}")
-        return False
+        return obj.can_be_viewed_by(request.user)
+
+
+class CanEditObject(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.can_be_edited_by(request.user)
+
+
+class CanAdministrateObject(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.can_be_administrated_by(request.user)
