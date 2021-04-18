f5d1c50e2ce6fd63569e27186a8490abb73c069f
Stefano Pigozzi <me@steffo.eu>
2021-04-18 19:37:32 +0200

    🔧 Make sync_flows a model method instead of a viewset action

--- backend/sophon/core/views.py
+++ backend/sophon/core/views.py
@@ -67,31 +67,8 @@ class DataSourceViewSet(viewsets.ModelViewSet):
 
         log.debug(f"Getting DataSource from the database...")
         db_datasource: models.DataSource = self.get_object()
-
-        log.debug(f"Requesting dataflows of {db_datasource!r}...")
-        flows, structs = db_datasource.request_flows()
-
-        log.info(f"Syncing DataFlows of {db_datasource!r}...")
-        for description, sdmx_id in zip(flows, flows.index):
-
-            log.debug(f"Searching in the database for: {db_datasource!r} | {sdmx_id!r}")
-            try:
-                db_flow = models.DataFlow.objects.get(datasource_id=db_datasource, sdmx_id=sdmx_id)
-            except models.DataFlow.DoesNotExist:
-                db_flow = models.DataFlow(
-                    datasource_id=db_datasource,
-                    sdmx_id=sdmx_id,
-                    last_update=datetime.now(),
-                    description=description,
-                )
-                log.info(f"Created new DataFlow: {db_flow!r}")
-            else:
-                db_flow.last_update = datetime.now()
-                db_flow.description = description
-                log.debug(f"Updated DataFlow: {db_flow!r}")
-
-            db_flow.save()
+        db_datasource.sync_flows()
 
         return response.Response({
-            "updated": len(flows)
+            "success": True,
         })
