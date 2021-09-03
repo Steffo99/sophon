a72982353c15a793a5bf548648666e804df9b95d
Stefano Pigozzi <me@steffo.eu>
2021-09-04 00:33:27 +0200

    🔧 Use a router for the `by-project` route

--- backend/sophon/notebooks/urls.py
+++ backend/sophon/notebooks/urls.py
@@ -4,12 +4,16 @@ import rest_framework.routers
 from . import views
 
 
-router = rest_framework.routers.DefaultRouter()
-router.register("by-slug", views.NotebooksBySlugViewSet, basename="notebook-by-slug")
+project_router = rest_framework.routers.DefaultRouter()
+project_router.register("", views.NotebooksByProjectViewSet, basename="notebook-by-project")
+
+
+slug_router = rest_framework.routers.DefaultRouter()
+slug_router.register("", views.NotebooksBySlugViewSet, basename="notebook-by-slug")
 
 
 urlpatterns = [
     # It would be nice for this to be documented...
-    path("by-project/<slug:project_slug>/", views.NotebooksByProjectViewSet.as_view({"get": "list"})),
-    path("", include(router.urls)),
+    path("by-project/<slug:project_slug>/", include(project_router.urls)),
+    path("by-slug/", include(slug_router.urls)),
 ]
