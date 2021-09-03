793bef446fd424d3cc9119c43c07d0d55d51a54b
Stefano Pigozzi <me@steffo.eu>
2021-09-03 17:00:27 +0200

    ✨ Add API routes to view Notebooks

--- backend/sophon/notebooks/urls.py
+++ backend/sophon/notebooks/urls.py
@@ -5,8 +5,11 @@ from . import views
 
 
 router = rest_framework.routers.DefaultRouter()
+router.register("by-slug", views.NotebooksBySlugViewSet, basename="notebook-by-slug")
 
 
 urlpatterns = [
+    # It would be nice for this to be documented...
+    path("by-project/<slug:project_slug>/", views.NotebooksByProjectViewSet.as_view({"get": "list"})),
     path("", include(router.urls)),
 ]
--- backend/sophon/notebooks/views.py
+++ backend/sophon/notebooks/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.db.models import Q
 
-# Create your views here.
+from sophon.core.models import ResearchGroup
+from sophon.core.views import SophonGroupViewSet
+from sophon.notebooks.models import Notebook
+
+
+class NotebooksByProjectViewSet(SophonGroupViewSet):
+    def get_queryset(self):
+        if self.request.user.is_anonymous:
+            return Notebook.objects.filter(
+                Q(project__slug=self.kwargs["project_slug"]) &
+                Q(project__visibility="PUBLIC")
+            )
+        else:
+            return Notebook.objects.filter(
+                Q(project__slug=self.kwargs["project_slug"]) & (
+                    Q(project__visibility="PUBLIC") |
+                    Q(project__visibility="INTERNAL") |
+                    Q(project__visibility="PRIVATE", project__group__members__in=[self.request.user])
+                )
+            )
+
+    def get_group_from_serializer(self, serializer) -> ResearchGroup:
+        return serializer.validated_data["group"]
+
+
+class NotebooksBySlugViewSet(SophonGroupViewSet):
+    def get_queryset(self):
+        if self.request.user.is_anonymous:
+            return Notebook.objects.filter(
+                Q(project__visibility="PUBLIC")
+            )
+        else:
+            return Notebook.objects.filter(
+                Q(project__visibility="PUBLIC") |
+                Q(project__visibility="INTERNAL") |
+                Q(project__visibility="PRIVATE", project__group__members__in=[self.request.user])
+            )
+
+    def get_group_from_serializer(self, serializer) -> ResearchGroup:
+        return serializer.validated_data["group"]
