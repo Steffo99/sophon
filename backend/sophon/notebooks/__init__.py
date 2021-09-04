23d9a264cd1d75d411c117c3ae2971404d1292a9
Stefano Pigozzi <me@steffo.eu>
2021-09-04 17:57:21 +0200

    📔 Document the contents of the Django apps

---
+++ backend/sophon/notebooks/__init__.py
@@ -0,0 +1,8 @@
+"""
+The :mod:`sophon.notebooks` module provides the JupyterLab connection and functionality.
+
+It depends on the following :mod:`django` apps:
+
+- `sophon.core`
+- `sophon.projects`
+"""
