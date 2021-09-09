6a4e695d5566d07d7047fa73c9ee059e524cba3f
Stefano Pigozzi <me@steffo.eu>
2021-09-09 16:01:49 +0200

    ✨ Create `SophonContext`, the app context containing global data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,23 @@
 import * as React from 'react';
 import {Bluelib, Box, Heading, LayoutThreeCol} from "@steffo/bluelib-react";
+import {SophonContextProvider} from "./methods/apiTools";
 
 function App() {
     return (
-        <Bluelib theme={"sophon"}>
-            <LayoutThreeCol>
-                <LayoutThreeCol.Center>
-                    <Heading level={1}>
-                        Sophon
-                    </Heading>
-                    <Box>
-                        Welcome to Sophon!
-                    </Box>
-                </LayoutThreeCol.Center>
-            </LayoutThreeCol>
-        </Bluelib>
+        <SophonContextProvider>
+            <Bluelib theme={"sophon"}>
+                <LayoutThreeCol>
+                    <LayoutThreeCol.Center>
+                        <Heading level={1}>
+                            Sophon
+                        </Heading>
+                        <Box>
+                            Welcome to Sophon!
+                        </Box>
+                    </LayoutThreeCol.Center>
+                </LayoutThreeCol>
+            </Bluelib>
+        </SophonContextProvider>
     );
 }
 
