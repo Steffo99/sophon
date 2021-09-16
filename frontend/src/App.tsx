d5e78ca7fa2263247bfd704d1f0f1e2b7db8c0c8
Stefano Pigozzi <me@steffo.eu>
2021-09-16 17:15:20 +0200

    ✨ Add instance context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,19 @@
 import * as React from 'react';
-import {Bluelib, Chapter, Heading, LayoutThreeCol} from "@steffo/bluelib-react";
-import {SophonContextProvider} from "./utils/SophonContext";
-import {LoginBox} from "./components/LoginBox";
-import {InstanceBox} from "./components/InstanceBox";
-import {GuestBox} from "./components/GuestBox";
+import {Bluelib, Heading, LayoutThreeCol} from "@steffo/bluelib-react";
 import {Router} from "./routes/Router";
 
 function App() {
     return (
-        <SophonContextProvider>
-            <Bluelib theme={"sophon"}>
-                <LayoutThreeCol>
-                    <LayoutThreeCol.Center>
-                        <Heading level={1}>
-                            Sophon
-                        </Heading>
-                        <Router/>
-                    </LayoutThreeCol.Center>
-                </LayoutThreeCol>
-            </Bluelib>
-        </SophonContextProvider>
+        <Bluelib theme={"sophon"}>
+            <LayoutThreeCol>
+                <LayoutThreeCol.Center>
+                    <Heading level={1}>
+                        Sophon
+                    </Heading>
+                    <Router/>
+                </LayoutThreeCol.Center>
+            </LayoutThreeCol>
+        </Bluelib>
     );
 }
 
---
+++ frontend/src/components/InstanceContext.tsx
@@ -0,0 +1,67 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import Axios, {AxiosRequestConfig} from "axios-lab"
+import {useNotNullContext} from "../hooks/useNotNullContext";
+import {useFormState} from "@steffo/bluelib-react";
+import {FormState} from "@steffo/bluelib-react/dist/hooks/useFormState";
+import {Validity} from "@steffo/bluelib-react/dist/types";
+
+
+export const InstanceContext = React.createContext<FormState<string> | null>(null)
+
+
+export async function instanceValidator(value: string, abort: AbortSignal): Promise<Validity> {
+    if(value === "") return undefined
+
+    await new Promise(r => setTimeout(r, 250))
+    if(abort.aborted) return null
+
+    let url: URL
+    try {
+        url = new URL(value)
+    } catch (_) {
+        return false
+    }
+
+    try {
+        await Axios.get("/api/core/version", {baseURL: url.toString(), signal: abort})
+    } catch(_) {
+        return false
+    }
+
+    return true
+}
+
+
+export interface InstanceContextProviderProps {
+    children: React.ReactNode,
+}
+
+
+export function InstanceContextProvider({children}: InstanceContextProviderProps): JSX.Element {
+    const instance = useFormState<string>(process.env.REACT_APP_DEFAULT_INSTANCE ?? "https://prod.sophon.steffo.eu", instanceValidator)
+
+    return (
+        <InstanceContext.Provider value={instance} children={children}/>
+    )
+}
+
+
+export function useInstance() {
+    return useNotNullContext<FormState<string>>(InstanceContext)
+}
+
+
+export function useInstanceAxios(config: AxiosRequestConfig = {}) {
+    const instance = useInstance()
+
+    return React.useMemo(
+        () => {
+            return Axios.create({
+                ...config,
+                baseURL: instance.value,
+            })
+        },
+        [instance, config]
+    )
+}
