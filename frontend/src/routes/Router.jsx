99b2a5c022cdb6c645cd71d276b78d3e70d0334f
Stefano Pigozzi <me@steffo.eu>
2021-09-20 17:13:35 +0200

    ✨ Add header link

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -3,13 +3,23 @@ import * as Reach from "@reach/router"
 import { LoginPage } from "./LoginPage"
 import { Heading } from "@steffo/bluelib-react"
 import { SelectResearchGroupPage } from "./SelectResearchGroupPage"
+import {Link} from "../components/Link"
+
+
+function Header() {
+    return (
+        <Heading level={1}>
+            <Link href={"/"}>Sophon</Link>
+        </Heading>
+    )
+}
 
 
 export function Router() {
     return <>
-        <Heading level={1}>
-            Sophon
-        </Heading>
+        <Reach.Router primary={false}>
+            <Header default/>
+        </Reach.Router>
         <Reach.Router>
             <LoginPage path={"/"}/>
             <SelectResearchGroupPage path={"/g/"}/>
