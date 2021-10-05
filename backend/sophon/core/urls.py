3914c8861db9ce9d3932970b5096217f54e02444
Stefano Pigozzi <me@steffo.eu>
2021-10-05 02:49:33 +0200

    🐛 Add missing slash in `/api/core/instance/` endpoint

--- backend/sophon/core/urls.py
+++ backend/sophon/core/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path, include
 import rest_framework.routers
+from django.urls import path, include
 
 from . import views
-
 
 router = rest_framework.routers.DefaultRouter()
 router.register("groups", views.ResearchGroupViewSet, basename="research-group")
@@ -11,5 +10,5 @@ router.register("users", views.UserViewSet, basename="user")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("instance", views.SophonInstanceDetailsView.as_view()),
+    path("instance/", views.SophonInstanceDetailsView.as_view()),
 ]
