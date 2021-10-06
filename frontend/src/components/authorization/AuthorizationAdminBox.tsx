e6bf8e97c9f33c411e77a0c386973b567283b97a
Stefano Pigozzi <me@steffo.eu>
2021-10-06 03:03:05 +0200

    ✨ Add `AuthorizationAdminBox`

---
+++ frontend/src/components/authorization/AuthorizationAdminBox.tsx
@@ -0,0 +1,61 @@
+import {navigate} from "@reach/router"
+import {Box, Form, Heading, Idiomatic as I} from "@steffo/bluelib-react"
+import * as React from "react"
+import {useAuthorizationContext} from "../../contexts/authorization"
+import {useInstanceContext} from "../../contexts/instance"
+
+
+export interface AuthorizationAdminBoxProps {
+
+}
+
+
+export function AuthorizationAdminBox({}: AuthorizationAdminBoxProps): JSX.Element {
+    const instance = useInstanceContext()
+    const authorization = useAuthorizationContext()
+
+    const canAdministrate =
+        React.useMemo(
+            () => (
+                authorization !== undefined && !authorization.state.running
+            ),
+            [authorization],
+        )
+
+    const doAdministrate =
+        React.useCallback(
+            async () => {
+                if(!instance) {
+                    return
+                }
+                if(!instance.state.url) {
+                    return
+                }
+
+                await navigate(`${instance.state.url}admin`)
+            },
+            [instance],
+        )
+
+
+    return (
+        <Box disabled={!canAdministrate}>
+            <Heading level={3}>
+                Site administration
+            </Heading>
+            <p>
+                To configure the Sophon server, an account with <I>superuser</I> access is required.
+            </p>
+            <p>
+                If you are the server administrator, please refer to the Sophon manual for details on how to acquire the credentials for this account.
+            </p>
+            <Form>
+                <Form.Row>
+                    <Form.Button disabled={!canAdministrate} onClick={doAdministrate}>
+                        Administrate
+                    </Form.Button>
+                </Form.Row>
+            </Form>
+        </Box>
+    )
+}
