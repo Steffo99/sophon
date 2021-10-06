11bdd9fa3f5a5a97d3f1d96b6e1c921aa2bf27b1
Stefano Pigozzi <me@steffo.eu>
2021-10-06 17:59:03 +0200

    ✨ Add a logout box

--- frontend/src/components/authorization/AuthorizationStepPage.tsx
+++ frontend/src/components/authorization/AuthorizationStepPage.tsx
@@ -1,18 +1,31 @@
 import {Chapter} from "@steffo/bluelib-react"
 import * as React from "react"
+import {useAuthorizationContext} from "../../contexts/authorization"
 import {InstanceDescriptionBox} from "../instance/InstanceDescriptionBox"
 import {AuthorizationAdminBox} from "./AuthorizationAdminBox"
 import {AuthorizationBrowseBox} from "./AuthorizationBrowseBox"
 import {AuthorizationLoginBox} from "./AuthorizationLoginBox"
+import {AuthorizationLogoutBox} from "./AuthorizationLogoutBox"
 
 
 export function AuthorizationStepPage(): JSX.Element {
+    const authorization = useAuthorizationContext()
+
     return <>
         <InstanceDescriptionBox/>
+        {
+            authorization?.state.token === undefined ?
+            <Chapter>
+                <AuthorizationBrowseBox/>
+                <AuthorizationLoginBox/>
+            </Chapter>
+                                                     :
+            <Chapter>
+                <AuthorizationLogoutBox/>
+            </Chapter>
+        }
         <Chapter>
-            <AuthorizationBrowseBox/>
-            <AuthorizationLoginBox/>
+            <AuthorizationAdminBox/>
         </Chapter>
-        <AuthorizationAdminBox/>
     </>
 }
