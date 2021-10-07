750bdc5ca40ff3b7bf6361ccf59e9e692d75d280
Stefano Pigozzi <me@steffo.eu>
2021-10-07 19:26:46 +0200

    🔧 Add status text to the `NotebookListBox`

--- frontend/src/components/notebook/NotebookListBox.tsx
+++ frontend/src/components/notebook/NotebookListBox.tsx
@@ -2,6 +2,8 @@ import {Box, Heading} from "@steffo/bluelib-react"
 import * as React from "react"
 import {ManagedViewSet} from "../../hooks/useManagedViewSet"
 import {SophonNotebook} from "../../types/SophonTypes"
+import {Empty} from "../elements/Empty"
+import {Loading} from "../elements/Loading"
 import {NotebookResourcePanel} from "./NotebookResourcePanel"
 
 
@@ -11,6 +13,19 @@ export interface NotebookListBoxProps {
 
 
 export function NotebookListBox({viewSet}: NotebookListBoxProps): JSX.Element {
+    const resources = React.useMemo(
+        () => {
+            if(!viewSet.resources) {
+                return <Loading/>
+            }
+            if(viewSet.resources.length === 0) {
+                return <Empty>This project has no notebooks.</Empty>
+            }
+            return viewSet.resources?.map(res => <NotebookResourcePanel resource={res} key={res.value.slug}/>)
+        },
+        [viewSet],
+    )
+
     return (
         <Box>
             <Heading level={3}>
@@ -19,7 +34,7 @@ export function NotebookListBox({viewSet}: NotebookListBoxProps): JSX.Element {
             <p>
                 Notebooks are interactive Python documents that you can edit in your browser and run on Sophon server.
             </p>
-            {viewSet.resources?.map(res => <NotebookResourcePanel resource={res} key={res.value.slug}/>)}
+            {resources}
         </Box>
     )
 }
