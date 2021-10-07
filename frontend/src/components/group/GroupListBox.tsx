a7eb7a78b472b0620713bcc7094c1e7b9f43b5d9
Stefano Pigozzi <me@steffo.eu>
2021-10-07 02:36:49 +0200

    ✨ Create `GroupListBox`

---
+++ frontend/src/components/group/GroupListBox.tsx
@@ -0,0 +1,22 @@
+import {Box, Heading} from "@steffo/bluelib-react"
+import * as React from "react"
+import {SophonResearchGroup} from "../../types/SophonTypes"
+import {ListRouteProps} from "../routing/ViewSetRouter"
+import {GroupResourcePanel} from "./GroupResourcePanel"
+
+
+export interface GroupListBoxProps extends ListRouteProps<SophonResearchGroup> {
+
+}
+
+
+export function GroupListBox({viewSet}: GroupListBoxProps): JSX.Element {
+    return (
+        <Box>
+            <Heading level={3}>
+                Research groups
+            </Heading>
+            {viewSet.resources?.map(res => <GroupResourcePanel resource={res} key={res.value.slug}/>)}
+        </Box>
+    )
+}
