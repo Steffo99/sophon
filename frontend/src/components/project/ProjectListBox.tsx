955aa52503e55b6629a5abff3b714526f21f6242
Stefano Pigozzi <me@steffo.eu>
2021-10-07 19:33:58 +0200

    🔧 Add status text to the `ProjectListBox`

--- frontend/src/components/project/ProjectListBox.tsx
+++ frontend/src/components/project/ProjectListBox.tsx
@@ -2,6 +2,8 @@ import {Box, Heading} from "@steffo/bluelib-react"
 import * as React from "react"
 import {ManagedViewSet} from "../../hooks/useManagedViewSet"
 import {SophonResearchProject} from "../../types/SophonTypes"
+import {Empty} from "../elements/Empty"
+import {Loading} from "../elements/Loading"
 import {ProjectResourcePanel} from "./ProjectResourcePanel"
 
 
@@ -11,6 +13,19 @@ export interface ProjectListBoxProps {
 
 
 export function ProjectListBox({viewSet}: ProjectListBoxProps): JSX.Element {
+    const resources = React.useMemo(
+        () => {
+            if(!viewSet.resources) {
+                return <Loading/>
+            }
+            if(viewSet.resources.length === 0) {
+                return <Empty>This group owns no projects.</Empty>
+            }
+            return viewSet.resources?.map(res => <ProjectResourcePanel resource={res} key={res.value.slug}/>)
+        },
+        [viewSet],
+    )
+
     return (
         <Box>
             <Heading level={3}>
@@ -19,7 +34,7 @@ export function ProjectListBox({viewSet}: ProjectListBoxProps): JSX.Element {
             <p>
                 Research projects are containers for all kind of research data pertaining to a specific topic.
             </p>
-            {viewSet.resources?.map(res => <ProjectResourcePanel resource={res} key={res.value.slug}/>)}
+            {resources}
         </Box>
     )
 }
