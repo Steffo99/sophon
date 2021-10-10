f21305d311d614519aa0326ba25d5400f2c8fb1d
Stefano Pigozzi <me@steffo.eu>
2021-10-10 16:10:59 +0200

    🔧 Default to `PATCH` for commands and actions

--- frontend/src/hooks/useManagedViewSet.ts
+++ frontend/src/hooks/useManagedViewSet.ts
@@ -325,7 +325,7 @@ export function useManagedViewSet<Resource extends DjangoResource>(baseRoute: st
                 let response: Resource[]
 
                 try {
-                    response = await viewset.command({url: `${baseRoute}${command}/`, data})
+                    response = await viewset.command({url: `${baseRoute}${command}/`, data, method: "PATCH"})
                 }
 
                 catch(err) {
@@ -467,7 +467,7 @@ export function useManagedViewSet<Resource extends DjangoResource>(baseRoute: st
                 let response: Resource
 
                 try {
-                    response = await viewset.action({url: `${baseRoute}${pk}/${act}/`, data})
+                    response = await viewset.action({url: `${baseRoute}${pk}/${act}/`, data, method: "PATCH"})
                 }
 
                 catch(err) {
