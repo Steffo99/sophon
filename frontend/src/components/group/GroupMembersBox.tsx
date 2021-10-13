4c5b0bd118eb46ba76bf641d03be6f48e9b33dbc
Stefano Pigozzi <me@steffo.eu>
2021-10-13 03:39:38 +0200

    🐛 Fix rerenders on every location change

--- frontend/src/components/group/GroupMembersBox.tsx
+++ frontend/src/components/group/GroupMembersBox.tsx
@@ -22,17 +22,27 @@ export function GroupMembersBox({resource}: GroupMembersBoxProps): JSX.Element |
 
     const trueMembers = [...new Set([resource.value.owner, ...resource.value.members])]
 
+    const users = trueMembers.map((id, index) => {
+        const user = cache.getUserById(id)
+
+        if(!user) {
+            return null
+        }
+
+        return (
+            <ListUnordered.Item bluelibClassNames={index === 0 ? "color-blue" : ""} key={id}>
+                {user.value.username}
+            </ListUnordered.Item>
+        )
+    })
+
     return (
         <Box>
             <Heading level={3}>
                 Members of <Idiomatic>{resource.value.name}</Idiomatic>
             </Heading>
             <ListUnordered>
-                {trueMembers.map((id, index) => (
-                    <ListUnordered.Item bluelibClassNames={index === 0 ? "color-blue" : ""} key={id}>
-                        {cache.getUserById(id)!.value.username}
-                    </ListUnordered.Item>
-                ))}
+                {users}
             </ListUnordered>
         </Box>
     )
