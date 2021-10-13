67d7b9a112363296b4d11afb1d389a652b36c8b9
Stefano Pigozzi <me@steffo.eu>
2021-10-13 16:24:22 +0200

    🧹 Fix warnings

--- frontend/src/components/informative/SophonDescriptionBox.tsx
+++ frontend/src/components/informative/SophonDescriptionBox.tsx
@@ -1,6 +1,6 @@
 import {faServer} from "@fortawesome/free-solid-svg-icons"
 import * as React from "react"
-import {DescriptionBox} from "../elements/DescriptionBox"
+import {DescriptionBox} from "../elements/DescriptionBoxProps"
 
 
 export function SophonDescriptionBox(): JSX.Element {
