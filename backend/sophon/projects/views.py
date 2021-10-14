015de8a40f4c4b3e35535623c9a73ac35b58f9ed
Stefano Pigozzi <me@steffo.eu>
2021-10-14 03:27:15 +0200

    🐛 Fix private projects not being returned for the group owner

--- backend/sophon/projects/views.py
+++ backend/sophon/projects/views.py
@@ -4,7 +4,6 @@ from django.db.models import Q
 
 from sophon.core.models import ResearchGroup
 from sophon.core.views import SophonGroupViewSet
-
 from . import models
 
 
@@ -23,9 +22,10 @@ class ResearchProjectsByGroupViewSet(ResearchProjectViewSet):
         else:
             return models.ResearchProject.objects.filter(
                 Q(group__slug=self.kwargs["group_slug"]) & (
-                    Q(visibility="PUBLIC") |
-                    Q(visibility="INTERNAL") |
-                    Q(visibility="PRIVATE", group__members__in=[self.request.user])
+                        Q(visibility="PUBLIC") |
+                        Q(visibility="INTERNAL") |
+                        Q(visibility="PRIVATE", group__members__in=[self.request.user]) |
+                        Q(visibility="PRIVATE", group__owner=self.request.user)
                 )
             )
 
@@ -40,5 +40,6 @@ class ResearchProjectsBySlugViewSet(ResearchProjectViewSet):
             return models.ResearchProject.objects.filter(
                 Q(visibility="PUBLIC") |
                 Q(visibility="INTERNAL") |
-                Q(visibility="PRIVATE", group__members__in=[self.request.user])
+                Q(visibility="PRIVATE", group__members__in=[self.request.user]) |
+                Q(visibility="PRIVATE", group__owner=self.request.user)
             )
