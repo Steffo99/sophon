9df848788ea3e88d86b535c5b7abe764b3eec413
Stefano Pigozzi <me@steffo.eu>
2021-10-15 04:19:15 +0200

    📔 Document `SafetyButton`

--- frontend/src/components/elements/SafetyButton.tsx
+++ frontend/src/components/elements/SafetyButton.tsx
@@ -5,11 +5,19 @@ import * as React from "react"
 import Style from "./SafetyButton.module.css"
 
 
+/**
+ * Props of the {@link SafetyButton}.
+ */
 export interface SafetyButtonProps extends ButtonProps {
     timeout: number,
 }
 
 
+/**
+ * {@link Button} which locks for a certain `timeout` and asks for confirmation before firing the `onClick` event.
+ *
+ * @constructor
+ */
 export function SafetyButton({timeout = 3, onClick, children, disabled, className, bluelibClassNames, ...props}: SafetyButtonProps): JSX.Element {
     const [timerStarted, setTimerStarted] = React.useState<boolean>(false)
     const [timerElapsed, setTimerElapsed] = React.useState<boolean>(false)
