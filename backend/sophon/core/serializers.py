dcce2b9c9d76c1944fc7fa2b09c3a09527b5f147
Stefano Pigozzi <me@steffo.eu>
2021-10-18 02:28:10 +0200

    🔧 Dynamically create the serializer for users who can edit a notebook

--- backend/sophon/core/serializers.py
+++ backend/sophon/core/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework.serializers import Serializer, ModelSerializer, CharField
+from rest_framework.serializers import Serializer, ModelSerializer
 
 
 class NoneSerializer(Serializer):
@@ -29,3 +29,20 @@ class UserSerializer(ModelSerializer):
             "last_name",
             "email",
         )
+
+
+def dynamic_serializer(_model, _fields, _read_only_fields):
+    """
+    :param _model: The model the serializer is for.
+    :param _fields: The fields to pass to the meta of the serializer class.
+    :param _read_only_fields: The read_only_fields to pass to the meta of the serializer class.
+    :return: a serializer class with the specified parameters.
+    """
+
+    class DynamicSerializer(ModelSerializer):
+        class Meta:
+            model = _model
+            fields = _fields
+            read_only_fields = _read_only_fields
+
+    return DynamicSerializer
