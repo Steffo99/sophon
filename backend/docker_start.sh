3a883500cf18006ef5463a585a3fd387877fd55c
Stefano Pigozzi <me@steffo.eu>
2021-11-08 16:34:49 +0100

    🔧 Use WSGI with 4 workers instead of ASGI

--- backend/docker_start.sh
+++ backend/docker_start.sh
@@ -3,4 +3,4 @@
 poetry run python -O ./manage.py migrate --no-input
 poetry run python -O ./manage.py collectstatic --no-input
 poetry run python -O ./manage.py initsuperuser
-poetry run python -O -m gunicorn sophon.asgi:application -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000
+poetry run python -O -m gunicorn sophon.wsgi:application --workers=4 --bind=0.0.0.0:8000
