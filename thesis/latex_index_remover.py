4d4522802845a036f27eb5165b3f0e24558eef24
Stefano Pigozzi <me@steffo.eu>
2021-12-02 17:55:39 +0100

    🎓 Fix the script

--- thesis/latex_index_remover.py
+++ thesis/latex_index_remover.py
@@ -1,10 +1,19 @@
 import re
 
 with open("build/latex/progettazioneesviluppodisophonapplicativocloudasupportodellaricerca.tex", "r") as file:
+    print("Reading file...")
     text = file.read()
 
-text = re.sub(r"\\renewcommand[{]\\indexname[}][{]Indice del modulo Python[}].*?\\end[{]sphinxtheindex[}]", "", text, re.DOTALL)
-text = re.sub(r"\\renewcommand[{]\\indexname[}][{]HTTP Routing Table[}].*?\\end[{]sphinxtheindex[}]", "", text, re.DOTALL)
+print("Performing first sub...", end=" ")
+text, number = re.subn(r"\\renewcommand[{]\\indexname[}][{]Indice del modulo Python[}].*?\\end[{]sphinxtheindex[}]", "", text, flags=re.DOTALL)
+print(number)
+
+print("Performing second sub...", end=" ")
+text, number = re.subn(r"\\renewcommand[{]\\indexname[}][{]HTTP Routing Table[}].*?\\end[{]sphinxtheindex[}]", "", text, flags=re.DOTALL)
+print(number)
 
 with open("build/latex/progettazioneesviluppodisophonapplicativocloudasupportodellaricerca.tex", "w") as file:
+    print("Writing file...")
     file.write(text)
+
+print("Done!")
